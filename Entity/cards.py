d0a05946bd6b4ddb54831d9599a0f26076290ea9
ArghyaAB <Arghya@VAIO>
2021-10-06 11:38:40 +0530

    Add a primitive sprite

--- Entity/cards.py
+++ Entity/cards.py
@@ -1,6 +1,15 @@
-class Card:
+import arcade
+
+
+class Card(arcade.Sprite):
     """Base class for all the cards to derive from"""
 
-    def __init__(self, name: str = "Primitive card", type: str = "Primitive"):
+    def __init__(
+        self,
+        name: str = "Primitive card",
+        type: str = "Primitive",
+        sprite: str = "assets/primitive.png",
+    ):
         self.name = name
         self.type = type
+        super().__init__(f":resources:{sprite}")
